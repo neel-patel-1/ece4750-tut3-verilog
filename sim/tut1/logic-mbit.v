092c303042ea74229f7662d8f0eba20fa843bae7
nmp83 <nmp83@cornell.edu>
2025-11-13 13:30:58 -0500

    shift

--- sim/tut1/logic-mbit.v
+++ sim/tut1/logic-mbit.v
@@ -4,9 +4,18 @@ module top;
   logic c;
 
   initial begin
+    // Exercise: Implementing Equality
     A = 4'b0111; $display("A=4'b%b", A);
     B = 4'b0110; $display("B=4'b%b", B);
     c = ~(|(A ^ B)); $display( "4'b%b == 4'b%b: %x", A, B, c);
+
+    // Shifting intricacies: Logical shift shifts in zero, arithmetic right shift replicates MSB, left-hand operand to arithmetic shift must be "signed"
+    B = A << 1; $display( "4'b0111 << 1 = %b", B);
+    B = A <<< 1; $display( "4'b0111 <<< 1 = %b", B);
+    A = 4'b1100;
+    B = A >>> 1; $display( "4'b1100 >>> 1 = %b", B);
+    B = $signed(A) >>> 1; $display( "$signed(4'b1100) >>> 1 = %b", B);
+
   end
 
 endmodule
