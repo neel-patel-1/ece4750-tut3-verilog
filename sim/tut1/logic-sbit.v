6c87909c2089e0c5c1040636ef5a413d5ac0ba10
nmp83 <nmp83@cornell.edu>
2025-11-12 19:59:19 -0500

    full adder

--- sim/tut1/logic-sbit.v
+++ sim/tut1/logic-sbit.v
@@ -1,11 +1,17 @@
 module top;
   logic a;
   logic b;
-  logic c;
+  logic c_in;
+  logic c_out;
+  logic s;
 
   initial begin
     a = 1'b0; $display( "1'b0 = %x ", a );
-    a = 1'b1; $display( "1'b1 = %x ", a );
+    b = 1'b1; $display( "1'b1 = %x ", b );
+    c_in = 1'b0; $display( "1'b0 = %x ", c_in );
+
+    s = a ^ b ^ c_in; $display( "1'b(a^b) = %x ", s );
+    c_out = (a & b) | (b & c_in) | (a & c_in); $display( "1'b(a&b) = %x ", c_in );
   end
 
 endmodule
