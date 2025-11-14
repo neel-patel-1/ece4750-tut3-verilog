65e18398d69fdf56b2cfe3853453809318d3456e
nmp83 <nmp83@cornell.edu>
2025-11-13 20:02:43 -0500

    ternary

--- sim/tut1/logic-ternary.v
+++ sim/tut1/logic-ternary.v
@@ -21,6 +21,23 @@ module top;
 
     $display( "sel = 1, c = %x", c );
 
+    sel = 2'bxx;
+    c = (sel == 2'b00) ? 8'h0a
+      : (sel == 2'b01) ? 8'h0b
+      : (sel == 2'b10) ? 8'h0c
+      : (sel == 2'b11) ? 8'h0d
+      : 8'h0e;
+
+    $display( "sel = xx, c = %b", c );
+
+    sel = 2'bx0;
+    c = (sel == 2'b00) ? 8'h0a
+      : (sel == 2'b01) ? 8'h0b
+      : (sel == 2'b10) ? 8'h0c
+      : (sel == 2'b11) ? 8'h0d
+      : 8'h0e;
+
+    $display( "sel = 0x, c = %b", c );
 
   end
 
