a42da99c073280409a70cb0077584e8bbaef6008
nmp83 <nmp83@cornell.edu>
2025-11-14 12:50:31 -0500

    reg_incr init

---
+++ regincr/reg_incr.v
@@ -0,0 +1,14 @@
+`ifndef ECE4750_TUT3_VERILOG_REGINCR_REG_INCR_V
+`define ECE4750_TUT3_VERILOG_REGINCR_REG_INCR_V
+
+module ece4750_tut3_verilog_regincr_Reg_Incr
+(
+  input logic clk,
+  input logic reset,
+  input logic [7:0] in_,
+  output logic [7:0] out
+);
+
+endmodule
+
+`endif // ECE4750_TUT3_VERILOG_REGINCR_REG_INCR_V
