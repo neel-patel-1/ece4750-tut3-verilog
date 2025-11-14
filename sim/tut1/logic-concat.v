f62900199c3f46ada6f6d148c2c036af9fc7b1d3
nmp83 <nmp83@cornell.edu>
2025-11-13 19:14:04 -0500

    multi concat

--- sim/tut1/logic-concat.v
+++ sim/tut1/logic-concat.v
@@ -2,11 +2,13 @@ module top;
   logic [3:0] A;
   logic [3:0] B;
   logic [7:0] C;
+  logic [15:0] D;
 
   initial begin
     A = 4'ha; $display("A=4'b%b", A);
     B = 4'hb; $display("B=4'b%b", B);
     C = {A, B}; $display("{ 8'hf0, 4'ha } = %x", C);
+    D = {B, {3{A}}}; $display("{ B, 3{A} } = %x", D);
 
 
   end
