ef722479c71698ad881226c9f105962ea8899412
nmp83 <nmp83@cornell.edu>
2025-11-14 17:26:22 -0500

    add a register and a wire

--- regincr/reg_incr.v
+++ regincr/reg_incr.v
@@ -9,6 +9,21 @@ module ece4750_tut3_verilog_regincr_Reg_Incr
   output logic [7:0] out
 );
 
+  logic [7:0] reg_out;
+  always_ff @( posedge clk ) begin
+    if ( reset )
+      reg_out <= '0; // all bits zero
+    else
+      reg_out <= in_;
+  end
+
+  logic [7:0] temp_wire;
+  always_comb begin
+    temp_wire = reg_out + 1;
+  end
+
+  assign out = temp_wire;
+
 endmodule
 
 `endif // ECE4750_TUT3_VERILOG_REGINCR_REG_INCR_V
