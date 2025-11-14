e997b6cae05011e75894261b24ead44c58729201
nmp83 <nmp83@cornell.edu>
2025-11-13 19:27:47 -0500

    enum

---
+++ sim/tut1/logic-enum.v
@@ -0,0 +1,24 @@
+typedef enum logic [$clog2(4)-1:0] {
+  STATE_A,
+  STATE_B,
+  STATE_C,
+  STATE_D
+} state_t;
+
+module top;
+  // Declare variables
+
+  state_t state;
+  logic result;
+
+  initial begin
+    state = STATE_A; $display("STATE_A = %d", state);
+    state = STATE_B; $display("STATE_B = %d", state);
+    state = STATE_C; $display("STATE_C = %d", state);
+    state = STATE_D; $display("STATE_D = %d", state);
+
+    result = (state == STATE_C); $display("state == STATE_C: result=%b", result);
+  end
+
+
+endmodule
