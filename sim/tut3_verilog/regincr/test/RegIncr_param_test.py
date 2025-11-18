cce511fbf70958ecf90db6061e38e1736a21b858
nmp83 <nmp83@cornell.edu>
2025-11-17 19:19:47 -0500

    change rand value to peak at 2**n

--- sim/tut3_verilog/regincr/test/RegIncr_param_test.py
+++ sim/tut3_verilog/regincr/test/RegIncr_param_test.py
@@ -16,7 +16,7 @@ def test_random( n, cmdline_opts ):
   test_vector_table = [( 'in_   out*' )]
   last_result = '?'
   for _ in range(20):
-    rand_value = Bits8( randint(0,0xff))
+    rand_value = Bits(n, randint(0,2**n - 1))
     test_vector_table.append( [rand_value, last_result] )
-    last_result = Bits8( rand_value + 1, trunc_int=True)
+    last_result = Bits(n, rand_value + 1, trunc_int=True)
   run_test_vector_sim( RegIncr(), test_vector_table, cmdline_opts )
